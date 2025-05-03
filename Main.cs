5dc7da56961fff1ed9507c41c435f592b8b18f0d
elliot! <benjikrolfanpageuwu@gmail.com>
2025-05-03 10:12:13 +0100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace GoToScary
+{
+    [BepInPlugin("elliot.gorillatag", "HorrorSpawn", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            if (PlayerPrefs.HasKey("seenGhostReactor"))
+                PlayerPrefs.DeleteKey("seenGhostReactor");
+        }
+    }
+}
+
